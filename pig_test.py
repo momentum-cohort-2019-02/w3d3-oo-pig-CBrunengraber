426b4f5e1a0b71428c8e399f93fef7b7755dd522
Craig Brunengraber <brunengraber@hotmail.com>
2019-02-28 11:39:29 -0500

    updated test, down to one error

--- pig_test.py
+++ pig_test.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 from pig import Human, Computer_player, Game
 
 def test_human():
@@ -12,9 +11,10 @@ def test_computer_player():
     assert my_computer_player.score == 0    
 
 
+# how do i get the init method to work with this test - check the video from the morning at 11:20 - shapes.py & testshapes references from Clinton
 
-def test_game():
-    my_test_game = Game(Human, Computer_player) #this test should assert if this is true, but self not defined and i cant pass the test an argument above
+def test_game(self):
+    my_test_game = Game(Human, Computer_player) 
     assert self.human == Human()
     assert self.computer_player == Computer_player()
 
